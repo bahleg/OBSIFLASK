e40165dcc2d013ecb386b3f849669d017c32ac0f
bahleg <bahleg@bahleg.ru>
2025-08-30 21:21:19 +0200

    Fix

--- flobsidian/pages/messages.py
+++ flobsidian/pages/messages.py
@@ -4,12 +4,15 @@ from flobsidian.pages.index_tree import render_tree
 from flobsidian.singleton import Singleton
 from datetime import datetime
 
-def render_messages(vault, unread, raw = False):
+
+def render_messages(vault, unread, raw=False):
     messages = get_messages(vault, unread=unread)
     if raw:
         return jsonify(messages)
-    return render_template('messages.html', home = Singleton.config.vaults[vault].home_file, 
-                           messages = messages,
-                           navtree=render_tree(Singleton.indices[vault], vault, False),
-                           vault = vault, unread=bool(unread)
-                           )
+    return render_template('messages.html',
+                           home=Singleton.config.vaults[vault].home_file,
+                           messages=messages,
+                           navtree=render_tree(Singleton.indices[vault], vault,
+                                               False),
+                           vault=vault,
+                           unread=bool(unread))
