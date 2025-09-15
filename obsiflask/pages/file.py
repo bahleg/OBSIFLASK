56f5a72f5c803fb5ef742dbf178e6877ad903a98
bahleg <bahleg@bahleg.site>
2025-09-15 12:39:38 +0200

    Test for file downloading

--- obsiflask/pages/file.py
+++ obsiflask/pages/file.py
@@ -1,9 +1,17 @@
+"""
+The module provides a download link for files in the vault
+"""
 from pathlib import Path
-from obsiflask.app_state import AppState
-from flask import send_file, abort
+from flask import send_file
 
 
-def get_file(real_path):
+def get_file(real_path: str):
+    """
+    Retruns a download response for the path
+
+    Args:
+        real_path (str): absolute path
+    """
     return send_file(real_path,
                      as_attachment=True,
                      download_name=Path(real_path).name)
