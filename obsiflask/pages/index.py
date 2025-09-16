0755610c5573d586700f7e6dd53a6accb0152c04
bahleg <bahleg@bahleg.ru>
2025-09-16 13:06:24 +0200

    Vault index

--- obsiflask/pages/index.py
+++ obsiflask/pages/index.py
@@ -1,5 +1,12 @@
+"""
+A simple module with vault list
+"""
 from flask import render_template
 from obsiflask.app_state import AppState
 
+
 def render_index():
-    return render_template('index.html', vaults = AppState.config.vaults)
+    """
+    Returns a list of vauls in rendered form
+    """
+    return render_template('index.html', vaults=AppState.config.vaults)
